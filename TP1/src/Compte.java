74ee363a1ee16c993597825f090a07191eeb6653
majdsassi <jackreider997@gmail.com>
2025-09-17 09:44:31 +0100

    Fix Synatx Errors

--- TP1/src/Compte.java
+++ TP1/src/Compte.java
@@ -1,24 +1,30 @@
+package TP1 ;
 public class Compte {
-    float solde ;
-    int nc ;
-    public void Deposer(float x ) {
-        solde += x ;
-    }
-    public void Retrait(float x ) {
-        if (x > solde) {
-            solde -= x ;
+        float solde ;
+        int nc ;
+        public void deposer(float x) {
+            solde += x;
         }
-        else{
-            System.out.Println("Solde inssufucsant ") ;
+        
+        public void retrait(float x) {
+            if (x <= solde) {
+                solde -= x;
+            } else {
+                System.out.println("Solde insuffisant");
+            }
         }
-    }
-    public ConsulterSolde(){
-        System.out.Println("Votre Solde est : ", solde) ;
-    }
-    public void Verier(Compte ac, float x ) {
-        Retrait(x) ;
-        ac.Deposer(x)
-    
-    }
+        
+        public void consulterSolde() {
+            System.out.println("Votre Solde est : " + solde);
+        }
+        
+        public void virer(Compte ac, float x) {
+            if (x <= solde) {
+                retrait(x);
+                ac.deposer(x);
+            } else {
+                System.out.println("Solde insuffisant pour le virement");
+            }
+        }
 
 }
---
+++ TP1/src/main.java
@@ -0,0 +1,7 @@
+import TP1.Compte ; 
+class Main {
+    public static void main(String[] args) {
+        Compte c = new Compte(1000.02f, 123456789);
+        c.consulterSolde();
+    }
+}
